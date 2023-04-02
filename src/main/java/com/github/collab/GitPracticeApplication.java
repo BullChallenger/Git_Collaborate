205821b715026303da8856e74071e356c0232b48
BullChallenger <am23am45@naver.com>
2023-04-02 21:02:05 +0900

    first commit

---
+++ src/main/java/com/github/collab/GitPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.github.collab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/collab/GitPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.collab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
